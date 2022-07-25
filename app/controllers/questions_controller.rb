458585eac8fc2b87f36ff8f5616f00b809c45896
Shimodaira0910 <hirokiso83@gmail.com>
2022-07-25 03:10:57 +0000

    model及びdbの作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,30 +1,30 @@
 class QuestionsController < ApplicationController
   #質問一覧表示
-  def index_questions
+  def index
   end
   
   #質問詳細ページ表示
-  def show_detail
+  def detail
   end
   
   #質問の作成
-  def show_create
+  def create
   end
   
   #質問の登録
-  def show_register
+  def register
   end
   
   #質問の編集
-  def show_edit
+  def edit
   end
   
   #質問の更新
-  def show_update
+  def update
   end
   
   #質問の削除
-  def show_destroy
+  def destroy
   end
   
 end
